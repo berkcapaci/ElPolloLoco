ef983ff94bce285b3d6a1fdab2f74098c838a280
BerkCapaci <berkcapaci@hotmail.com.tr>
2026-07-15 08:59:54 +0200

    basics like bg adds

--- js/game.js
+++ js/game.js
@@ -1,17 +1,12 @@
 let canvas;
-let character = new Image();
-let ctx;
-
+let world;
 
 
 function init(){
     canvas = document.getElementById('canvas');
-    ctx = canvas.getContext('2d');
+    world = new World(canvas);
+    
 
-    character.src = '../img/2_character_pepe/2_walk/W-21.png';
-
-    setTimeout(function(){
-        ctx.drawImage(character, 20, 20, 50, 150);
-
-    }, 2000);    
+    console.log('My character is', world.character);
+    
 }
